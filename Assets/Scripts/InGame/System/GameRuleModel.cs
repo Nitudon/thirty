af1fd1d9df5141962f3b1bad5c738139fd366d5b
udon <naoki_udon@outlook.jp>
2018-07-31 23:50:44 +0900

    create game rule model

---
+++ Assets/Scripts/InGame/System/GameRuleModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UdonLib.Commons;
+
+public class GameRuleModel : MonoBehaviour, IInitializable
+{
+    private ReactiveProperty<float> _gameSpeed;
+    public IReadOnlyReactiveProperty<float> GameSpeed => _gameSpeed;
+
+    private ReactiveProperty<float> _gameLife;
+    public IReadOnlyReactiveProperty<float> GameLife => _gameLife;
+
+    public void Initialize()
+    {
+        _gameSpeed = new ReactiveProperty<float>();
+        _gameLife = new ReactiveProperty<float>();
+    }
+}
