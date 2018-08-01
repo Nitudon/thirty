9912d4b1f6ca0309c17e56b5adff003adf477312
udon <naoki_udon@outlook.jp>
2018-08-01 22:58:54 +0900

    impl system

--- Assets/Scripts/InGame/Player/Model/PlayerModel.cs
+++ Assets/Scripts/InGame/Player/Model/PlayerModel.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 using UdonLib.Commons;
 
 
-public class PlayerModel
+public class PlayerModel : UdonBehaviour
 {
+    [SerializeField]
+    private Collider2D _playerCollision;
+    public Collider2D PlayerCollision => _playerCollision;
+
     private readonly FriendCollectionModel _friends;
 
 
