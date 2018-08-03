4d8c15f012ba68a20282ff8d16bdb414d1bfc75a
udon <naoki_udon@outlook.jp>
2018-08-04 04:07:26 +0900

    create system presenter

---
+++ Assets/Scripts/InGame/System/Presenter/GameRuleSystemPresenter.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UdonLib.Commons;
+
+public class GameRuleSystemPresenter : MonoBehaviour {
+
+    [SerializeField]
+    private List<IInitializable> _initializables;
+
+    [SerializeField]
+    private List<IAsyncInitializable> _asyncInitializables;
+
+    private async void Start()
+    {
+        _initializables.ForEach(x => x.Initialize());
+        await Task.WhenAll(_asyncInitializables.Select(x => x.Initialize()));
+    }
+}
