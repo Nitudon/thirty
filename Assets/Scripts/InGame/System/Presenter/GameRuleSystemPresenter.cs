e114a210ea522bc6f0090a553cbad3750fe0a52b
udon <naoki_udon@outlook.jp>
2018-08-09 02:09:54 +0900

    mpl system presenter

--- Assets/Scripts/InGame/System/Presenter/GameRuleSystemPresenter.cs
+++ Assets/Scripts/InGame/System/Presenter/GameRuleSystemPresenter.cs
@@ -5,15 +5,15 @@ using System.Linq;
 using UnityEngine;
 using UdonLib.Commons;
 
-public class GameRuleSystemPresenter : MonoBehaviour {
-
+public class GameRuleSystemPresenter : UdonBehaviour
+{
     [SerializeField]
     private List<InitializableMono> _initializables;
 
     [SerializeField]
     private List<AsyncInitializableMono> _asyncInitializables;
 
-    private async void Start()
+    protected override async void Start()
     {
         _initializables.ForEach(x => x.Initialize());
         await Task.WhenAll(_asyncInitializables.Select(x => x.Initialize()));
