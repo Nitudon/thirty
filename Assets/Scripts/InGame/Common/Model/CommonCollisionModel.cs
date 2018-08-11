8e34c2df14680bbf9e98986daf1643e387fe934a
udon <naoki_udon@outlook.jp>
2018-08-12 02:50:05 +0900

    common collision model

---
+++ Assets/Scripts/InGame/Common/Model/CommonCollisionModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonLib.Commons;
+using UniRx;
+using UniRx.Triggers;
+using Thirty.Enum;
+
+[RequireComponent(typeof(Collider2D))]
+public class CommonCollisionModel : UdonBehaviour 
+{
+    [SerializeField]
+    private Collider2D _collider;
+
+    private CollisionType _collisionType;
+    public CollisionType CollisionType => _collisionType;
+
+    private IObservable<CollisionType> _onCollisionModelEnterAsObservable;
+    public IObservable<CollisionType> OnCollisionModelEnterAsObservable => _onCollisionModelEnterAsObservable;
+
+    public void SetCollisionType(CollisionType collision)
+    {
+        _collisionType = collision;
+    }
+
+    public void SetObserver()
+    {
+        _onCollisionModelEnterAsObservable =
+            _collider
+                .OnTriggerEnter2DAsObservable()
+                .Where(x => x != null)
+                .Select(_ => _collisionType);
+    }
+}
