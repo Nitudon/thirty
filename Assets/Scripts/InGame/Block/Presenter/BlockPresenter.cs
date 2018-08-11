95a1e0e2e05f3f473fd5826d445ed995683d7b1d
udon <naoki_udon@outlook.jp>
2018-08-12 02:51:27 +0900

    create block presenter with collision

---
+++ Assets/Scripts/InGame/Block/Presenter/BlockPresenter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonLib.Commons;
+
+public class BlockPresenter : InitializableMono
+{
+    [SerializeField]
+    private BlockView _blockView;
+
+    private CommonCollisionModel _collisionModel;
+
+    public override void Initialize()
+    {
+        _collisionModel.SetObserver();       
+    }
+}
