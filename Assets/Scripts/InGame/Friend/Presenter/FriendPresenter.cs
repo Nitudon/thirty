db43766054501ab7b75527814b326a2b932759a1
udon <naoki_udon@outlook.jp>
2018-08-14 01:00:18 +0900

    create friend layer

---
+++ Assets/Scripts/InGame/Friend/Presenter/FriendPresenter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonLib.Commons;
+
+public class FriendPresenter : InitializableMono
+{
+    [SerializeField]
+    private FriendView _friendView;
+
+    private CommonCollisionModel _collisionModel;
+
+    public override void Initialize()
+    {
+        _collisionModel.SetObserver();
+    }
+}
---
+++ Assets/Scripts/InGame/Friend/View/FriendView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonLib.UI;
+
+public class FriendView : UIMono
+{
+    [SerializeField]
+    private SimpleAnimation _friendAnimator;
+
+    public void PlayAnimation()
+    {
+
+    }
+}
