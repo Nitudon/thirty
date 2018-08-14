444b4529da92a6572b76a806657f84bfac59ae4a
udon <naoki_udon@outlook.jp>
2018-08-15 01:38:50 +0900

    impl collision observe layer

---
+++ Assets/Scripts/InGame/Player/UseCase/Player/PlayerCollisionUseCase.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UniRx;
+using Zenject;
+
+public class PlayerCollisionUseCase
+{
+    [Inject]
+    private InGameCollisionModel _inGameCollisionModel;
+    private CommonCollisionModel _playerCollisionModel;
+
+    private CompositeDisposable _collisionDisposable;
+
+    public PlayerCollisionUseCase(CommonCollisionModel model)
+    {
+        _playerCollisionModel = model;
+    }
+
+    public void BindCollision()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InGame/System/Model/InGameCollisionModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonLib.Commons;
+using UniRx;
+using Thirty.Data;
+
+public class InGameCollisionModel : InitializableMono
+{
+    public Subject<FriendData> OnCollisionFriend;
+    public Subject<BlockData> OnCollisionBlock;
+
+    public override void Initialize()
+    {
+        OnCollisionFriend = new Subject<FriendData>();
+        OnCollisionBlock = new Subject<BlockData>();
+    }
+}
